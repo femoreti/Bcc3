ee96eb4b5b1f7f87314617dd8595815a5da041dd
Caio Rosa <caiolrosa1912@gmail.com>
2017-03-05 01:16:35 -0300

    Add: Objeto posto funcional

--- Unity/PI3-Sistema Autonomo/Assets/Scripts/FileReader/FileReader.cs
+++ Unity/PI3-Sistema Autonomo/Assets/Scripts/FileReader/FileReader.cs
@@ -3,24 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 using System;
+using UnityEditor;
 
 public class FileReader : MonoBehaviour {
 	public int qtdAtendentes = 0;
-	List<string> relPostos = new List<string>();
-	List<string> relAtendentes = new List<string>();
-	List<string> tempoPostos = new List<string>();
+	public List<Posto> postos;
 
-	public void ReadFile() {
-		string txt = File.ReadAllText("test.txt");
+	private string relPostos;
+	private string relAtendentes;
+	private List<string> tempoPostos = new List<string>();
+	void Start() {
+		ReadTextFile();
+		postos = PopulaObjetoPosto();
+	}
+
+	private void ReadTextFile() {
+		string txt = File.ReadAllText(Application.dataPath + "/StreamingAssets/Setup.txt");
+		
+		if (txt == null || txt == "") {
+			throw new Exception("Não foi possível ler a entrada.");
+		}
+
 		string[] txtArray = txt.Split('\n');
 		qtdAtendentes = Convert.ToInt16(txtArray[0]);
 
 		foreach (var item in txtArray) {
 			var temp = item.Split(':');
 			if (temp[0] == "RP") {
-				relPostos.Add(temp[1].Replace("\r", ""));
+				relPostos = temp[1].Replace("\r", "");
 			} else if (temp[0] == "RA") {
-				relAtendentes.Add(temp[1].Replace("\r", ""));
+				relAtendentes = temp[1].Replace("\r", "");
 			} else {
 				tempoPostos.Add(temp[0]);
 			}
@@ -28,5 +40,36 @@ public class FileReader : MonoBehaviour {
 
 		tempoPostos.RemoveAt(0);
 		tempoPostos.Remove("TROCA");
+		tempoPostos.Remove("");
+	}
+	private List<Posto> PopulaObjetoPosto() {
+		Dictionary<char, int> postos = new Dictionary<char, int>();
+		List<Posto> postosList = new List<Posto>();
+
+		for (int i = 0; i < relPostos.Length; i++) {
+			if (!postos.ContainsKey(relPostos[i])) {
+				postos.Add(relPostos[i], 1);
+			} else {
+				postos[relPostos[i]]++;
+			}
+		}
+	
+		foreach (var item in postos) {
+			postosList.Add(new Posto(item.Key, 0, item.Value));
+		}
+
+		foreach (var item in tempoPostos) {
+			var letra = item.Substring(0, 1);
+			var turnos = item.Substring(1, item.Length - 1);
+			
+			foreach (var posto in postosList) {
+				if (posto.letra.ToString() == letra) {
+					posto.turnos = Convert.ToInt16(turnos);
+					break;
+				}
+			}
+		}
+
+		return postosList;
 	}
 }
---
+++ Unity/PI3-Sistema Autonomo/Assets/Scripts/FileReader/Posto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Posto {
+	public char letra { get; set; }
+	public int turnos { get; set; }
+	public int quantidade { get; set; }
+
+	public Posto(char letra, int turnos, int quantidade)
+	{
+		this.letra = letra;
+		this.turnos = turnos;
+		this.quantidade = quantidade;
+	}
+}
